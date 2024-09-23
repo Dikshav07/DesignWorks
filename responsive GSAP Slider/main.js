125b10117c2f0f9eb4a56cecd04bcf12f49a000e
Diksha Verma <150647851+Dikshav07@users.noreply.github.com>
2024-09-23 23:54:49 +0530

    Add files via upload

---
+++ responsive GSAP Slider/main.js
@@ -0,0 +1,99 @@
+let h1Texts = ["Pear", "Apple", "Exotic"]; 
+let logoColors = [
+  "var(--pear-logo)",
+  "var(--apple-logo)",
+  "var(--exotic-logo)"
+]; 
+let keyframes = ["wave-pear-effect", "wave-apple-effect", "wave-exotic-effect"]; 
+// Normal GSAP animation.......
+gsap.from(".fruit-image ", { y: "-100vh", delay: 0.5 });
+gsap.to(".fruit-image img", {
+  x: "random(-20, 20)",
+  y: "random(-20, 20)",
+  zIndex: 22,
+  duration: 2,
+  ease: "none",
+  yoyo: true,
+  repeat: -1
+});
+
+// getting the elements
+const waveEffect = document.querySelector(".wave");
+const sections = document.querySelectorAll(".section");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+const caneLabels = document.querySelector(".cane-labels");
+const sectionContainer = document.querySelector(".section-container");
+// Setting index and current position
+let index = 0;
+let currentIndex = 0;
+let currentPosition = 0;
+
+// Adding the event listeners to the buttons
+nextButton.addEventListener("click", () => {
+  // here we are decresing the current position by 100% (to the left)
+  if (currentPosition > -200) {
+    currentPosition -= 100;
+    // Update the left position of the cane-labels
+    caneLabels.style.left = `${currentPosition}%`;
+    sectionContainer.style.left = `${currentPosition}%`;
+  }
+  // Increment index and currentIndex
+  currentIndex++;
+  // Update the h1 text if currentIndex is less than the length of h1Texts
+  if (currentIndex < h1Texts.length) {
+    document.querySelector(".h1").innerHTML = h1Texts[currentIndex];
+  }
+  // Gasp animation for next section components
+  gsap.to(".logo", {
+    opacity: 1,
+    duration: 1,
+    color: logoColors[currentIndex]
+  });
+  gsap.from(".h1", { y: "20%", opacity: 0, duration: 0.5 });
+  gsap.from(".fruit-image ", { y: "-100vh", delay: 0.4, duration: 0.4 });
+
+  // Disable the nextButton if the last section is active
+  if (currentIndex === h1Texts.length - 1) {
+    nextButton.style.display = "none";
+  }
+  // Enable the prevButton if it's not the first section
+  if (currentIndex > 0) {
+    prevButton.style.display = "block";
+  }
+  // Button colors and animations
+  nextButton.style.color = logoColors[currentIndex + 1];
+  prevButton.style.color = logoColors[currentIndex - 1];
+  nextButton.style.animationName = keyframes[currentIndex + 1];
+  prevButton.style.animationName = keyframes[currentIndex - 1];
+});
+// Add event listeners to the buttons
+prevButton.addEventListener("click", () => {
+  if (currentPosition < 0) {
+    currentPosition += 100;
+    // Update the left position of the cane-labels
+    caneLabels.style.left = `${currentPosition}%`;
+    sectionContainer.style.left = `${currentPosition}%`;
+    sectionContainer.style.transition = `all 0.5s ease-in-out`;
+  }
+  // Decrement index and currentIndex
+  currentIndex--;
+  if (currentIndex >= 0) {
+    document.querySelector(".h1").innerHTML = h1Texts[currentIndex];
+  }
+  // Gasp animation for previous section components
+  gsap.to(".logo", { color: logoColors[currentIndex], duration: 1 });
+  gsap.from(".h1", { y: "20%", opacity: 0, duration: 0.5 });
+  gsap.from(".fruit-image ", { y: "100vh", delay: 0.5 });
+  // Enable the nextButton if it was disabled
+  nextButton.style.display = "block";
+  // Disable the prevButton if it's the first section
+  if (currentIndex === 0) {
+    prevButton.style.display = "none";
+  }
+  // Button colors and animations
+  nextButton.style.color = logoColors[currentIndex + 1];
+  prevButton.style.color = logoColors[currentIndex - 1];
+  nextButton.style.animationName = keyframes[currentIndex + 1];
+  prevButton.style.animationName = keyframes[currentIndex - 1];
+});
